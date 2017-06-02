ca23dace41a7c2d18c04fa2b0b01a44c5a49959b
Arif <arif@convincebd.com>
2017-06-03 00:03:45 +0600

    Found bug that form validation not working with AngularJs

---
+++ App.Web/Controllers/TestsAPIController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using App.Core.Context;
+using App.Core.Models.EntityModels;
+
+namespace App.Web.Controllers
+{
+    public class TestsAPIController : ApiController
+    {
+        private AppDbContext db = new AppDbContext();
+
+        // GET: api/TestsAPI
+        public IQueryable<Test> GetTests()
+        {
+            return db.Tests;
+        }
+
+        // GET: api/TestsAPI/5
+        [ResponseType(typeof(Test))]
+        public IHttpActionResult GetTest(int id)
+        {
+            Test test = db.Tests.Find(id);
+            if (test == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(test);
+        }
+
+        // PUT: api/TestsAPI/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutTest(int id, Test test)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != test.TestId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(test).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TestExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/TestsAPI
+        [ResponseType(typeof(Test))]
+        public IHttpActionResult PostTest(Test test)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Tests.Add(test);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = test.TestId }, test);
+        }
+
+        // DELETE: api/TestsAPI/5
+        [ResponseType(typeof(Test))]
+        public IHttpActionResult DeleteTest(int id)
+        {
+            Test test = db.Tests.Find(id);
+            if (test == null)
+            {
+                return NotFound();
+            }
+
+            db.Tests.Remove(test);
+            db.SaveChanges();
+
+            return Ok(test);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TestExists(int id)
+        {
+            return db.Tests.Count(e => e.TestId == id) > 0;
+        }
+    }
+}
---
+++ App.Web/Scripts/app/test.js
@@ -0,0 +1,103 @@
+﻿var app = angular.module("dcbmsApp", []);
+
+app.run(function ($rootScope, $http) {
+    $rootScope.tests = {};
+    $http.get("/api/TestsAPI").then(function (response) {
+        $rootScope.testTypes = response.data;
+    });
+});
+
+app.controller("testController", function ($rootScope, $scope, $http, $filter) {
+
+    $scope.submit = function () {
+        if ($scope.addTestForm.$valid) {
+            $http.post("/api/TestsAPI", $scope.test) .then(function(response) {
+                toastr.options = {
+                    "closeButton": true,
+                    "debug": false,
+                    "newestOnTop": true,
+                    "progressBar": true,
+                    "positionClass": "toast-top-right",
+                    "preventDuplicates": true,
+                    "showDuration": "300",
+                    "hideDuration": "1000",
+                    "timeOut": "5000",
+                    "extendedTimeOut": "1000",
+                    "showEasing": "swing",
+                    "hideEasing": "linear",
+                    "showMethod": "fadeIn",
+                    "hideMethod": "fadeOut"
+                };
+                if (response.status === 201) {
+                    $rootScope.tests.push(response.data);
+
+                    $scope.addTestForm.$setPristine();
+                    $scope.addTestForm.$setUntouched();
+                    $scope.test = {};
+                    //notification
+
+                    toastr.success('Success!', 'Test added!');
+                    return;
+                }
+                toastr.error('Error!', 'Internal Problem! Try Again!');
+                return;
+            });
+        }
+    };
+
+    $scope.editTest = function (e) {
+        $scope.test = {};
+        if (e.test !== null) {
+            $rootScope.$broadcast("test", e.test);
+        }
+    };
+
+    $scope.confirmDelete = function (e) {
+        swal({
+            title: 'Confirmation',
+            text: "Are you sure want to delete the data?",
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes',
+            cancelButtonText: 'No'
+        }, function (isConfirmed) {
+            if (isConfirmed) {
+                $http.delete("/api/TestsAPI/" + e.test.TestId).then(function (response) {
+                    if (response.status === 200) {
+                        $rootScope.tests = $filter("filter")($rootScope.tests, function (item) {
+                            return item.TestId !== e.test.TestId;
+                        });
+                    }
+                });
+            }
+        });
+    };
+});
+
+app.controller("modalController", function ($rootScope, $scope, $http) {
+    $scope.test = {};
+    $scope.$on("test", function (e, data) {
+        $scope.test.TestId = data.TestId;
+        $scope.test.TestName = data.TestName;
+        $scope.test.Fee = data.Fee;
+        $scope.test.TestTypeId = data.TestTypeId;
+    });
+
+    $scope.saveChangesTest = function () {
+        $http.put("/api/TestsAPI/" + $scope.test.TestId, $scope.test).then(function (response) {
+            if (response.status === 204) {
+                $("#editTestModal").modal("hide"); //not appropriate use of angular (Jquery Used)
+
+                angular.forEach($rootScope.tests, function (item) {
+                    if (item.TestId === $scope.test.TestId) {
+                        item.TestName = $scope.test.TestName;
+                        item.Fee = $scope.test.Fee;
+                        item.TestTypeId = $scope.test.TestTypeId;
+                    }
+                });
+            }
+        });
+    };
+});
